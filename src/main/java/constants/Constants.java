a4300ef347479536b181c687dcb86cc7703049a1
AlexanderShirdin <cpt.blak@gmail.com>
2022-04-23 09:10:49 +0300

    Проект web

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -1,9 +1,11 @@
 package constants;
 
 public interface Constants {
-    // local
-     String DB_URL_CONNECTION = "jdbc:mysql://192.168.88.98:3306/students_27?user=DC&password=Hhhh888*";
+    // localHP
+    //     String DB_URL_CONNECTION = "jdbc:mysql://localhost:3306/students_27?user=root&password=Hhhh888*";
+    // localASUS
+    //     String DB_URL_CONNECTION = "jdbc:mysql://192.168.88.98:3306/students_27?user=DC&password=Hhhh888*";
     // remote
-//    String DB_URL_CONNECTION = "jdbc:mysql://127.0.0.1:3306/students_27?user=java27&password=Hhhh888*";
+           String DB_URL_CONNECTION = "jdbc:mysql://127.0.0.1:3306/students_27?user=java27&password=Hhhh888*";
 }
 
